d23fb53722113fb69b739533c6ab12324f6eb0f3
jitain1 <singhjitainj@gmail.com>
2023-01-04 23:27:16 +0530

    Show Board added

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -30,10 +30,20 @@ public class TicTacToe {
         }
     }
 	
+	static void showBoard(){
+        System.out.println(board[1] +" | "+ board[2] + " | " + board[3]);
+        System.out.println("----------");
+        System.out.println(board[4] +" | "+ board[5] + " | " + board[6]);
+        System.out.println("----------");
+        System.out.println(board[7] +" | "+ board[8] + " | " + board[9]);
+        System.out.println("********************************************");
+    }
+	
 	
 	public static void main(String[] args) {
 		createBoard();
 		assignSymbol();
+		showBoard();
 	}
 
 }
